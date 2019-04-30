1041c6b7028c75b7a77485f831ca73198daa9543
tsosvielle <trevor.sosvielle@gmail.com>
2019-04-30 13:24:41 -0400

    Troubleshoot

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://secure-tor-78756.herokuapp.com/',
+  production: 'https://whispering-fortress-91173.herokuapp.com/',
   development: 'http://localhost:4741/'
 }
 
